a0526c20111ebede8f9c6a71c10a7d0223707782
Key-Paing <key.etverdict@gmail.com>
2025-05-29 14:43:53 +0700

    Added gemini flash and 1.5pro

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ with col1:
         #Model Selection
         model_selection = st.selectbox(
             "Which Model would you like to test now?",
-            ("Gemini 2.5 Pro", "Gemini 2.5 Flash", "Llama 4", "Gemini 1.5 Pro"),
+            ("Gemini 2.5 Pro", "Gemini 2.5 Flash", "Llama 4", "Gemini 1.5 Pro","Gemini 2.0 Flash"),
             index=None,
             placeholder="Select Model ...",
             key="model_selection"
@@ -138,7 +138,8 @@ with col1:
             model_name = {
                 "Gemini 2.5 Flash": "gemini-2.5-flash-preview-04-17",
                 "Gemini 2.5 Pro": "gemini-2.5-pro-preview-05-06",
-                "Gemini 1.5 Pro": "google/gemini-1.5-pro-002",
+                "Gemini 1.5 Pro": "gemini-1.5-pro-001",
+                "Gemini 2.0 Flash": "gemini-2.0-flash-001"
             }.get(model_selection)
 
             model_name_fromGroq = {
